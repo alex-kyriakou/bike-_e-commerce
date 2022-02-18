0d04f2ac5211e9e0e05245f091828117f8f51839
Alexandros <alexandr.kyriakou@gmail.com>
2022-02-18 09:09:41 +0200

    Header on scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,17 @@
 const toggle = document.querySelector(".mobile-toggle i");
 const toggle_Menu = document.querySelector(".nav-menu");
 
+//  Open-Close toggle mobile menu
 toggle.addEventListener("click", () => {
   toggle_Menu.classList.toggle("active");
+});
+
+// shrink Header when scroll
+const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    header.classList.add("shrink");
+  } else {
+    header.classList.remove("shrink");
+  }
 });
